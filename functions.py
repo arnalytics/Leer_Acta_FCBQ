e9ce85dd3c2aa70f1d9b193b0f51f8504cc3f4f9
arnaubarrera <arnau6baroy@gmail.com>
2024-03-30 16:40:31 +0100

    Ya se saca el boxscore de un partido. Ahora faltará leer todas las actas y contruir la base de datos e ir actualizandola con los boxscore de los nuevos partidos

--- functions.py
+++ functions.py
@@ -1,28 +1,38 @@
 # %%
 import tabula
-
+import pandas as pd
+import numpy as np
 
 # %%
 
+
 def extraer_datos(acta):
+    """
+    Extrae los datos relevantes del acta.
+
+    Argumentos:
+    acta (str): Ruta del archivo PDF del acta.
+
+    Retorna:
+    equipo_A (Dataframe): Información sobre jugadores del equipo local (número, nombre, cuartos, faltas).
+    equipo_B (Dataframe): Información sobre jugadores del equipo visitante (número, nombre, cuartos, faltas).
+    PbP (Dataframe): Play by play digitalizado.
+    """
+
     # Coordenadas de las tablas que queremos extraer [superior, izq, inferior, der]
     area_equip_A = [140, 45, 355, 408]
     area_equip_B = [390, 45, 595, 408]
-    area_playBplay = [114, 402, 538, 754]
 
     # Extraer tablas del PDF y guardarlas en DataFrames
-    raw_A = tabula.read_pdf(acta, pages='all', area=area_equip_A);     raw_A = raw_A[0]
-    raw_B = tabula.read_pdf(acta, pages='all', area=area_equip_B);     raw_B = raw_B[0]
-    raw_pbp = tabula.read_pdf(acta, pages='all', area=area_playBplay); raw_pbp = raw_pbp[0]
+    raw_A = tabula.read_pdf(acta, pages='all', area=area_equip_A)[0];
+    raw_B = tabula.read_pdf(acta, pages='all', area=area_equip_B)[0];
+    PbP = leer_pbp(acta)
 
     # Limpiamos las tablas de los equipos
-    clean_A = limpiar_equipo(raw_A)
-    clean_B = limpiar_equipo(raw_B)
+    equipo_A = limpiar_equipo(raw_A)
+    equipo_B = limpiar_equipo(raw_B)
 
-    # Limpiamos el Play by Play
-    clean_pbp = limpiar_pbp(raw_pbp)
-
-    return clean_A, clean_B, clean_pbp
+    return equipo_A, equipo_B, PbP
 
 
 # Limpiamos la tabla de jugadoras de un equipo
@@ -41,6 +51,206 @@ def limpiar_equipo(raw_i):
     return clean_i
 
 
-def limpiar_pbp(raw_pbp):
+def leer_pbp(acta):
+    """
+    Lee la parte del Play by Play (Acumulació de punts) del acta.
 
-    return 1
+    Argumentos:
+    acta (str): Ruta del archivo PDF del acta.
+
+    Retorna:
+    PbP (Dataframe): Play by play digitalizado.
+    """
+
+    # Coordenadas tres primeros bloques (el 4o y el 5o suelen estar vacíos)
+    x1 = 402; x2 = 613
+    y1 = 115; y2 = 552
+    area = [y1, x1, y2, x2]
+    columns = [418, 430, 445, 460, 473, 485, 500, 515, 530, 543, 556, 572, 585, 600]
+
+    # Extraer los datos del PDF
+    raw_pbp = tabula.read_pdf(acta, pages='all', area=area, columns=columns, pandas_options={'header': None})[0]
+
+    # Reorganizamos los datos
+    titulos = ['Jug A', 'Anotación A', 'Min', 'Jug B', 'Anotación B']
+
+    raw_pbp_1 = raw_pbp.iloc[0:, 0:5].reset_index(drop=True)
+    raw_pbp_2 = raw_pbp.iloc[0:, 5:10].reset_index(drop=True)
+    raw_pbp_3 = raw_pbp.iloc[0:, 10:15].reset_index(drop=True)
+
+    # Ponemos títulos
+    raw_pbp_1.columns = titulos
+    raw_pbp_2.columns = titulos
+    raw_pbp_3.columns = titulos
+
+    # Creamos el dataframe limpio
+    PbP = pd.concat([raw_pbp_1, raw_pbp_2, raw_pbp_3], axis=0, ignore_index=True)
+    PbP = PbP.dropna(how='all')
+
+    # Eliminar filas de cambio de cuarto
+    caracteres_a_eliminar = ['---', '-', '===']
+    PbP = PbP[~PbP['Jug A'].isin(caracteres_a_eliminar)]
+
+    # Rellenamos los minutos en qué sucede cada jugada
+    PbP['Min'] = PbP['Min'].fillna(method='ffill').astype(object)
+
+    # Rellenamos los TL que no tienen lanzador
+    for index, row in PbP.iterrows():
+        if pd.isna(row['Jug A']) and pd.notna(row['Anotación A']):  # Equipo A
+            PbP.at[index, 'Jug A'] = PbP.at[index-1, 'Jug A']
+
+        if pd.isna(row['Jug B']) and pd.notna(row['Anotación B']):  # Equipo B
+            PbP.at[index, 'Jug B'] = PbP.at[index-1, 'Jug B']
+
+    return PbP
+
+
+def crea_boxscore(equipo_a, equipo_b):
+    """
+        Creamos el dataframe vacío con los títulos
+
+        Retorna:
+        Boxscore (Dataframe): Boxscore vacío. Número, Puntos, 2p, 3p, TL intentados, TL anotados
+    """
+    numeros = equipo_a['Número']
+    cuarto = equipo_a['Q1']
+    filas_vacias = (numeros.eq('--') | cuarto.eq('--'))  # Eliminamos las filas del entrenador
+    numeros_a = pd.to_numeric(numeros[~filas_vacias])
+    boxscore_A = pd.DataFrame({'Número': numeros_a},
+                              columns=['Número', 'Puntos', '2P', '3P', 'TL fallados', 'TL anotados'])
+
+    numeros = equipo_b['Número']
+    cuarto = equipo_b['Q1']
+    filas_vacias = (numeros.eq('--') | cuarto.eq('--'))
+    numeros_b = pd.to_numeric(numeros[~filas_vacias])
+    boxscore_B = pd.DataFrame({'Número': numeros_b},
+                              columns=['Número', 'Puntos', '2P', '3P', 'TL fallados', 'TL anotados'])
+
+    return boxscore_A, boxscore_B
+
+
+def tl_fallados(pbp, boxscore_a, boxscore_b):
+    """
+        Actualiza el boxscore con los TL fallados por cada jugadora
+
+        Argumentos:
+        pbp (Dataframe): Raw pbp que saca la función extraer_datos()
+        boxscore (Dataframe): Boxscore vacío.
+
+        Retorna:
+        pbp (Dataframe): Play by Play actualizado sin TL fallados
+        boxscore (Dataframe): Boxscore actualizado con los TL fallados (dorsal jugadora y cantidad de TL fallados)
+    """
+
+    # Encontrar las celdas que tienen el valor '-'
+    tl_fallados_A = pbp.loc[pbp['Anotación A'] == '-']
+    pbp = pbp.drop(tl_fallados_A.axes[0])  # Eliminamos los TL fallados del Play by Play
+    tl_fallados_A = tl_fallados_A[['Jug A']].value_counts()  # Cuenta los tiros libre fallados por cada jugadora
+
+    tl_fallados_B = pbp.loc[pbp['Anotación B'] == '-']
+    pbp = pbp.drop(tl_fallados_B.axes[0])
+    tl_fallados_B = tl_fallados_B[['Jug B', 'Anotación B']].value_counts()
+
+    # Metemos los TL fallados en el boxscore TODO: Optimizar
+    for fila in tl_fallados_A.index:
+        jugadora = int(fila[0])
+        fila_boxscore = (boxscore_a['Número'] == jugadora)
+        boxscore_a.loc[fila_boxscore, 'TL fallados'] = tl_fallados_A[fila]
+
+    for fila in tl_fallados_B.index:
+        jugadora = int(fila[0])
+        fila_boxscore = (boxscore_b['Número'] == jugadora)
+        boxscore_b.loc[fila_boxscore, 'TL fallados'] = tl_fallados_B[fila]
+
+    return pbp, boxscore_a, boxscore_b
+
+
+def tipo_anotacion(pbp):
+    """
+        Calcula la diferencia entre dos anotaciones consecutivas de los equipos para saber ...
+        qué tipo de tiro se ha anotado (2P, 3P, TL)
+
+        Argumentos:
+        pbp (Dataframe): Pbp limpio, sin los TL fallados
+
+        Retorna:
+        pbp (Dataframe): Play by Play con valor de la anotación
+    """
+
+    # Creamos una fila inicial con marcador 0-0
+    fila_0 = pd.DataFrame({'Jug A': [np.nan], 'Anotación A': [0], 'Min': [0], 'Jug B': [np.nan], 'Anotación B': [0]},
+                          index=[0])
+    pbp = pd.concat([fila_0, pbp]).reset_index(drop=True)
+
+    # Hacemos la diferencia entre anotaciones para saber de qué tipo ha sido la canasta (2p, 3p, TL)
+    pbp['Anotación A'] = pbp['Anotación A'].fillna(method='ffill')  # Cambiamos NaN por úiltimo valor anotado
+    pbp['Anotación A'] = pd.to_numeric(pbp['Anotación A'], errors='coerce')  # Convertimos las filas a números
+    pbp['Anotación A'] = pbp['Anotación A'].diff()  # Calcular diferencia entre la anotación actual y la acumulada
+
+    pbp['Anotación B'] = pbp['Anotación B'].fillna(method='ffill')
+    pbp['Anotación B'] = pd.to_numeric(pbp['Anotación B'], errors='coerce')
+    pbp['Anotación B'] = pbp['Anotación B'].diff()
+
+    return pbp
+
+
+def actualiza_boxscore(pbp, boxscore, equipo='A'):
+    """
+        Actualiza el boxscore con las anotaciones de las jugadoras del equipo seleccionado.
+
+        Argumentos:
+        pbp (Dataframe): Pbp con el tipo de anotaciónd e cada jugadora (el que saca la función tipo_anotacion() )
+        equipo (str): 'A', 'B'. Default 'A'. Equipo del que queremos sacar el boxscore
+
+        Retorna:
+        boxscore (Dataframe): Boxscore actualizado con el número de anotaciones de cada jugadora del equipo
+    """
+
+    # Sacamos el tipo de anotación para cada jugadora
+    if equipo == 'A':
+        anotaciones = pbp[['Jug A', 'Anotación A']].value_counts()
+    else:
+        anotaciones = pbp[['Jug B', 'Anotación B']].value_counts()
+
+    # Actualizamos el boxscore con estos datos
+    for fila in anotaciones.index:
+        jugadora = int(fila[0])
+        tipo_lanzamiento = fila[1]
+        fila_boxscore = (boxscore['Número'] == jugadora)
+        if tipo_lanzamiento == 2.0:
+            boxscore.loc[fila_boxscore, '2P'] = anotaciones[fila]
+        elif tipo_lanzamiento == 1.0:
+            boxscore.loc[fila_boxscore, 'TL anotados'] = anotaciones[fila]
+        elif tipo_lanzamiento == 3.0:
+            boxscore.loc[fila_boxscore, '3P'] = anotaciones[fila]
+
+    # Reemplazar NaN por 0
+    boxscore = boxscore.fillna(0).infer_objects(copy=False)
+
+    # Sumamos puntos totales
+    suma_puntos(boxscore)
+
+    return boxscore
+
+
+def suma_puntos(boxscore):
+    """
+    Lee el boxscore suma los puntos para cada jugadora.
+
+    Argumentos:
+    Boxscore (Dataframe): Boxscore de un equipo, con los tiros de 2P, 3P y TL rellenados, pero sin los puntos totales
+
+    Retorna:
+    Boxscore (Dataframe): Boxscore los puntos totales de cada jugadora
+    """
+
+    # Rellenar la columna de puntos totales
+    for index, jugadora in boxscore.iterrows():
+        dos  = jugadora['2P']
+        tres = jugadora['3P']
+        TL   = jugadora['TL anotados']
+
+        puntos = 2 * dos + 3 * tres + 1 * TL
+        boxscore.loc[index, 'Puntos'] = puntos
+
+    return boxscore
--- leer_acta.py
+++ leer_acta.py
@@ -1,78 +1,24 @@
-import pandas as pd
-import tabula
-import numpy as np
+import functions as fcbq
 
 # Archivo que vamos a leer
 acta = '/Users/arnaubarrera/Desktop/Sports Analyitics/Stats PreInf A/Actes/CNT vs Lima-Horta.pdf'
-#acta = '/Users/arnaubarrera/Desktop/Sports Analyitics/Stats PreInf A/Actes/CNT vs Xamba.pdf'
-#acta = '/Users/arnaubarrera/Desktop/Sports Analyitics/Stats PreInf A/Actes/Bufalà vs CNT.pdf'
+# acta = '/Users/arnaubarrera/Desktop/Sports Analyitics/Stats PreInf A/Actes/CNT vs Xamba.pdf'
+# acta = '/Users/arnaubarrera/Desktop/Sports Analyitics/Stats PreInf A/Actes/Bufalà vs CNT.pdf'
 
-# Coordenadas de las tablas A M B (hay 5 en total)
-x1 = 402; x2 = 473; x3 = 543; x4 = 613; x5 = 686; x6 = 754
-y1 = 115; y2 = 552
+# Leemos los datos del acta en PDF
+[equipo_A, equipo_B, raw_pbp] = fcbq.extraer_datos(acta)
 
-coordenadas_tablas = [
-    [y1, x1, y2, x2],
-    [y1, x2, y2, x3],
-    [y1, x3, y2, x4],
-    [y1, x4, y2, x5],
-    [y1, x5, y2, x6]
-]
+# Creamos Boxscore vacío (Número, Puntos, 2p, 3p, TL intentados, TL anotados)
+[boxscore_A, boxscore_B] = fcbq.crea_boxscore(equipo_A, equipo_B)
 
-raw_pbp = []
+# Actualizamos boxscore con TL fallados y los eliminamos del PbP
+[pbp, boxscore_A, boxscore_B] = fcbq.tl_fallados(raw_pbp, boxscore_A, boxscore_B)
 
-# TODO Cambiar la lectura de la tabla de anotación. Leer A1, M1, B1 por separado. Luego A2, M2, B2, ..., Ai, Mi, Bi
+# Limpiamos el PbP dejando solo el número de la jugadora y el tipo de anotación
+pbp = fcbq.tipo_anotacion(pbp)
 
-# Extraer las columnas de anotación A M B y guardar cada una en un DF distinto
-for area in coordenadas_tablas:
-    raw_pbp_i = tabula.read_pdf(acta, pages='all', area=area)
-    if raw_pbp_i:
-        raw_pbp_i = raw_pbp_i[0]
-        raw_pbp_i = raw_pbp_i.dropna(axis=1, how='all')  # Eliminamos columnas vacías
-        raw_pbp.append(raw_pbp_i)  # Añadimos los datos de la tabla a la lista global
-    else:
-        break
-
-# %% Crea un nuevo DataFrame con los títulos de las columnas como la primera fila
-
-# Define el orden deseado de las columnas
-orden_deseado = ['Jug. A', 'Anotación A', 'Min', 'Anotación B', 'Jug B']
-
-for i in range(0, len(raw_pbp)):
-    titulos_df = np.array(raw_pbp[i].columns)  # Valores de la primera fila de anotación
-
-    if titulos_df.shape[0] == 4:  # Tiene 4 columnas (i.e no ha leído bien la columna de minutos)
-        # Trabajamos c on la primera fila
-        primera_fila = pd.DataFrame([titulos_df], columns=['Jug. A', 'Anotación A', 'Anotación B', 'Jug B'])
-        raw_pbp[i].columns = ['Jug. A', 'Anotación A', 'Anotación B', 'Jug B']
-
-        # A veces lee mal el PDF y mete los minutos en otra columna, de forma que en total solo hay 4 cols.
-        a = raw_pbp[i]['Anotación A'].str.split(' ', expand=True)
-        if not a.iloc[:, 1].empty:
-            PbP['Anotación A'] = a.iloc[:, 0]
-            PbP['Min'] = a.iloc[:, 1]
-        else:
-            b = PbP['Anotación B'].str.split(' ', expand=True)
-
-            PbP['Anotación B'] = b.iloc[0]
-            PbP['Min'] = b.iloc[1]
-    elif titulos_df.shape[0] == 5:
-        primera_fila = pd.DataFrame([titulos_df], columns=['Jug. A', 'Anotación A', 'Min', 'Anotación B', 'Jug B'])
-        raw_pbp[i].columns = ['Jug. A', 'Anotación A', 'Min', 'Anotación B', 'Jug B']
-    else:
-        break
-
-    # Verifica si el orden actual de las columnas coincide con el orden deseado
-    if list(PbP.columns) != orden_deseado:
-        PbP = PbP.reindex(columns=orden_deseado)  # Reordena las columnas del DataFrame
-
-
-
-    primera_fila = primera_fila.apply(pd.to_numeric, errors='coerce')  # Comprobamos que son números. Si no, ponemos NaN
-    raw_pbp[i] = pd.concat([primera_fila, raw_pbp[i]], ignore_index=True)   # Concatena el nuevo DF con el DF original
-
-# Concatenamos verticalmente
-PbP = pd.DataFrame()
-PbP = pd.concat(raw_pbp, axis=0)
+# Actualizamos el boxscore con el número de anotaciones de cada jugadora
+boxscore_A = fcbq.actualiza_boxscore(pbp, boxscore_A, 'A')
+boxscore_B = fcbq.actualiza_boxscore(pbp, boxscore_B, 'B')
 
 print('Finished')
